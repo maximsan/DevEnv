3c240b8c331bbb6f312a2605a43ab5dfa06cff0c
= <maximuzz@yandex.ru>
2018-04-02 21:30:36 +0300

    Added eslintrc.json

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import './index.css';
 import numeral from 'numeral';
 
 const cValue = numeral(1000).format('$0.00');
-debugger;
-console.log(`I will pay ${cValue}`);
+debugger; //eslint-disable-line no-debugger
+console.log(`I will pay ${cValue}`); //eslint-disable-line no-console
