e2363a0f1e58c5c57b7ca200e65f802987c0c3fc
= <maximuzz@yandex.ru>
2018-04-06 22:40:06 +0300

    Added table, db fake data, fetch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import './index.css';
+import {getUsers} from './api/userApi';
 
-import numeral from 'numeral';
+// import numeral from 'numeral';
 
-const cValue = numeral(1000).format('$0.00');
-debugger; //eslint-disable-line no-debugger
-console.log(`I will pay ${cValue}`); //eslint-disable-line no-console
+// const cValue = numeral(1000).format('$0.00');
+// debugger; //eslint-disable-line no-debugger
+// console.log(`I will pay ${cValue}`); //eslint-disable-line no-console
+
+getUsers().then(result => {
+    let usersBody = "";
+
+    result.forEach(user => {
+        usersBody+= `<tr>
+            <td><a href="#" data-id=${user.id}" class = "deleteUser">Delete</a></td>
+            <td>${user.id}</td>
+            <td>${user.firstName}</td>
+            <td>${user.lastName}</td>
+            <td>${user.email}</td>
+        </tr>`
+    });
+
+    global.document.getElementById('users').innerHTML = usersBody;
+});
